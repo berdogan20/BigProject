2c8eda2e7068954a14bafd45ed9815a96b2a7cef
berdogan20 <berdogan20@ku.edu.tr>
2024-01-21 08:53:50 +0300

    Initial Commit

---
+++ BigProject/BigProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigProjectApp.swift
+//  BigProject
+//
+//  Created by beyza erdoğan on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BigProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
